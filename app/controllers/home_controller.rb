e892ec5b1ee1dcad6b76c169acd059aff7258fdb
Jin <nuker.jin@outlook.com>
2021-02-24 02:41:22 +0800

    Maintenance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,13 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!, :only => [:index]
 
   def index
-    # redirect_to "/maintenance.html"
-    product_shows = ProductShow.all.map do |ps|
-      ids = Project.where(creative_category: ps.creative_category_id).map(&:id)
-      ps.as_json.merge(ids: ids)
-    end
+    redirect_to "/maintenance.html"
+    # product_shows = ProductShow.all.map do |ps|
+    #   ids = Project.where(creative_category: ps.creative_category_id).map(&:id)
+    #   ps.as_json.merge(ids: ids)
+    # end
 
-    render component: 'HomePage', props: { banners: Banner.all, product_shows: product_shows }
+    # render component: 'HomePage', props: { banners: Banner.all, product_shows: product_shows }
   end
 
   def about_us
